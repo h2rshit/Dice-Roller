cca708ecd2e002cde82a88b8c2e4343a55725410
h2rshit <gbgb809423@gmail.com>
2024-06-04 14:32:59 +0530

    Update plan.java

--- plan.java
+++ plan.java
@@ -1,6 +1,3 @@
-M-0-> Prev DSA... (till 21st May)
-M-1-> JS complete from LB,BC,CJS && revise(M-1); (26,27,28th May)
-M-2-> PHP + SQL complete && project full stack && revise(M-2); (29,30,31st May)
 M-3-> DSA till Recursion & Backtracking && revise(M-0,M-1,M-2); (1st to 15th June)
 M-4-> React + Tailwind + Git && revise(M-0,M-1,M-2,M-3); (16th to 21st June)
 M-5-> DSA(oops & LL) && revise(M-0,M-1,M-2,M-3,M-4,M-5); (22nd June to 3rd July)
